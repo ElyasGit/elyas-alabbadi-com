24bbb255f3b14566c93d5a0973ffa40a1e3f6db7
ElyasGIT <18654417+ElyasGit@users.noreply.github.com>
2023-02-04 13:57:23 +0300

    First commit.

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Elyas Alabbadi - Software Engineer</title>
+        <meta name="description" content="TODO" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className=" bg">
+        <div >
+        <p>Start</p>
+        </div>
+      </main>
+    </div>
+  );
+}
